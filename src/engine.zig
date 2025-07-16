263685329e97a56af40e59acd7f61486975c41c9
diicell <diicellman@gmail.com>
2025-07-16 15:37:48 +0500

    smol upd

--- src/engine.zig
+++ src/engine.zig
@@ -2,12 +2,43 @@ const std = @import("std");
 
 pub const Value = struct {
     data: f32,
+    children: std.ArrayListUnmanaged(*const Value),
 
     pub fn init(data: f32) Value {
-        return Value{ .data = data };
+        return Value{
+            .data = data,
+            .children = .empty,
+        };
     }
 
-    pub fn add(self: Value, other: Value) Value {
-        return Value{ .data = self.data + other.data };
+    pub fn add(self: *const Value, other: *const Value, allocator: std.mem.Allocator) !Value {
+        var result = Value{ .data = self.data + other.data, .children = .empty };
+        try result.children.ensureUnusedCapacity(allocator, 2);
+        try result.children.append(allocator, self);
+        try result.children.append(allocator, other);
+        return result;
+    }
+
+    pub fn mul(self: *const Value, other: *const Value, allocator: std.mem.Allocator) !Value {
+        var result = Value{ .data = self.data * other.data, .children = .empty };
+        try result.children.ensureUnusedCapacity(allocator, 2);
+        try result.children.append(allocator, self);
+        try result.children.append(allocator, other);
+        return result;
+    }
+
+    // very shaky impl, maybe the future me will regret this
+    pub fn debug(self: Value) void {
+        std.debug.print("Value(data={d:.4}", .{self.data});
+        if (self.children.items.len > 0) {
+            std.debug.print(", children=[", .{});
+            for (self.children.items, 0..) |child, i| {
+                if (i > 0) std.debug.print(", ", .{});
+                std.debug.print("{d:.4}", .{child.data});
+            }
+            std.debug.print("]", .{});
+        }
+
+        std.debug.print(")\n", .{});
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,26 @@ const std = @import("std");
 const engine = @import("engine.zig");
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     const a = engine.Value.init(2.0);
     const b = engine.Value.init(-3.0);
-    const c = a.add(b);
-    std.debug.print("Value(data={d:.4})\n", .{a.data});
-    std.debug.print("Value(data={d:.4})\n", .{b.data});
-    std.debug.print("Value(data={d:.4})\n", .{c.data});
+    const c = engine.Value.init(10.0);
+
+    const temp = try a.mul(&b, allocator);
+    const result = try temp.add(&c, allocator);
+
+    a.debug();
+    b.debug();
+    c.debug();
+    result.debug();
+    // std.debug.print("the result is: {d:.4}\n", .{result.data});
 
     // i'll leave it here for now
     // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
